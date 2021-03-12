c16f4c5e5c15da8a235be3a868416e2c9b39170d
Manuela <manurod19@gmail.com>
2021-03-12 18:11:37 +0000

    add form that lets users contact me

---
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -0,0 +1,33 @@
+package com.google.sps.servlets;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/form-handler")
+public class FormHandlerServlet extends HttpServlet {
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    // Get the value entered in the form.
+    String textValue = request.getParameter("text-input");
+
+    // Print the value so you can see it in the server logs.
+    System.out.println("You submitted the email address: " + textValue);
+
+    // Write the value to the response so the user can see it.
+    response.getWriter().println("You submitted the email address: " + textValue);
+
+    // Get the value entered in the form.
+    String message = request.getParameter("message-input");
+
+    // Print the value so you can see it in the server logs.
+    System.out.println("You submitted the message: " + message);
+
+    // Write the value to the response so the user can see it.
+    response.getWriter().println("You submitted the message: " + message);
+  }
+}
